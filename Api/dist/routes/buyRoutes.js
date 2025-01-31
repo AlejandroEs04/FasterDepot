ea79d30e1027aee488c16b1633f554e93c210771
Raphael Estrada <2004.estrada.lopez@gmail.com>
2025-01-30 18:53:56 -0600

    'Some fixe'

--- Api/dist/routes/buyRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const buyController_1 = require("../controllers/buyController");
-const router = (0, express_1.Router)();
-router.post('/', buyController_1.BuyController.registerBuy);
-router.post('/payer', buyController_1.BuyController.getPayerBuy);
-exports.default = router;
