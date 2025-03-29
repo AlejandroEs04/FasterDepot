0ca11652533bd018df52e7374bb11a951ed664ae
Raphael Estrada <2004.estrada.lopez@gmail.com>
2025-03-28 20:43:09 -0600

    'Add cors'

--- Api/src/config/cors.ts
+++ Api/src/config/cors.ts
@@ -1,5 +1,9 @@
 import { CorsOptions } from 'cors';
 
+const allowedOrigins = ['https://www.fasterdepot.com']
+
 export const corsOptions: CorsOptions = {
-  origin: true
+  origin: allowedOrigins, 
+  methods: 'GET,POST,PUT,DELETE', 
+  allowedHeaders: 'Content-Type,Authorization'
 };
